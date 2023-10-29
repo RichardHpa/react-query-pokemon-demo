3cb39889c1261570a5cc9e89e2dcba4e431704f0
Richard <richard.m.hpa@gmail.com>
2023-10-29 19:57:32 +1300

    set up db file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // server/index.js
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,5 +9,16 @@ const PORT = 3001;
 // app.get('/api/users', (req, res) => {
 //   return res.json(users);
 // });
+
+// create your initial DB
+if (!fs.existsSync('./server/db.json')) {
+  const obj = {
+    table: [],
+  };
+  const json = JSON.stringify(obj);
+  fs.writeFile('./server/db.json', json, err => {
+    if (err) throw err;
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
